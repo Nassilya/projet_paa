7718b1b5458e47fc32d66ba2bb87f9721c58aeb8
Nassilya <belguedjnassilya@gmail.com>
2024-11-03 18:33:05 +0100

    Test

--- Projet_PAA_BELGUEDJ/src/app/monAppli.java
+++ Projet_PAA_BELGUEDJ/src/app/monAppli.java
@@ -1,11 +1,22 @@
 package app;
+import java.util.Scanner;
+
+import model.Colon;
+
 //This is url du dépot 
 //https://github.com/Nassilya/projet_paa.git
 public class monAppli {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Bienvenue cher commandant ! ");
+		System.out.println("Combien de colons souhaitez vous avoir dans votre colonie ? ");
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		
+	
+		
+sc.close();
 	}
 
 }
